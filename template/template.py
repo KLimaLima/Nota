5b451ce540e757d800ed5c068e0d44ef930f61d5
Khairul Ikhwan <khairul0355@hotmail.com>
2026-02-18 13:37:46 +0800

    setup and test Jinja2

---
+++ template/template.py
@@ -0,0 +1,31 @@
+from jinja2 import Environment, FileSystemLoader
+
+# max_score = 100
+# test_name = "Python Challenge"
+# students = [
+#     {"name": "Sandrine",  "score": 100},
+#     {"name": "Gergeley", "score": 87},
+#     {"name": "Frieda", "score": 92},
+# ]
+
+
+
+environment = Environment(loader=FileSystemLoader("template/"))
+template = environment.get_template("out.html")
+
+content = template.render()
+
+with open("result.html", mode="w", encoding="utf-8") as f:
+
+    f.write(content)
+
+# for student in students:
+#     filename = f"message_{student['name'].lower()}.txt"
+#     content = template.render(
+#         student,
+#         max_score=max_score,
+#         test_name=test_name
+#     )
+#     with open(filename, mode="w", encoding="utf-8") as message:
+#         message.write(content)
+#         print(f"... wrote {filename}")
