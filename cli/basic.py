face879cfa7a0855227913dc673a92fec4aa93a3
Khairul Ikhwan <khairul0355@hotmail.com>
2026-02-24 17:04:09 +0800

    move html file from dist to result

--- cli/basic.py
+++ cli/basic.py
@@ -16,12 +16,14 @@ def create_md(filename):
 # TODO: take md in notes and become html
 def create_nota(filename):
 
-    print(type(filename))
-
     parser.convert_md_html(filename)
 
     template.create_template(filename)
 
     template.render_template(filename)
 
-    ans = subprocess.check_output(["npm run build"], text=True, shell=True)
+    process_npm = subprocess.check_output(["npm run build"], text=True, shell=True)
+    print(process_npm)
+
+    process_mv = subprocess.check_output(["mv -v dist/index.html result/"], text=True, shell=True)
+    print(process_mv)
